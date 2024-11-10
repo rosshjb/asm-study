ee98178cc632ed670defd3d4638a5ec80cebea24
rosshjb <jinbeomhong@gmail.com>
2024-11-10 20:24:24 +0900

    add arm 15

---
+++ arm_smith/chapter15/upper.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int mytoupper(const char *instr, char *outstr) {
+    char *orig_outstr = outstr;
+
+    char cur;
+    do {
+        cur = *instr;
+        if ((cur >= 'a') && (cur <= 'z'))
+            cur = cur - ('a'-'A');
+        *outstr++ = cur;
+        instr++;
+    } while (cur != '\0');
+
+    return (outstr - orig_outstr);
+}
+
+#define BUFFERSIZE 250
+
+const char *tst_str ="This is a test!";
+char out_str[BUFFERSIZE];
+
+int main() {
+    mytoupper(tst_str, out_str);
+    printf("Input: %s\nOutput: %s\n", tst_str, out_str);
+
+    return 0;
+}
